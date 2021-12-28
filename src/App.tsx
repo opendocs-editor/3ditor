50277c084af95e9ad32a942c94333456e1943b38
Jacob Sapoznikow <jacob1coder@gmail.com>
2021-12-28 00:24:41 +0000

    Change sizes, add some more functionality & keybindings. Also add CI integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import { faBold, faItalic, faUnderline, faListUl, faListOl, faLink, faAlignLeft, faAlignRight, faAlignCenter, faAlignJustify, faImage } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { forEachTrailingCommentRange } from 'typescript';
-import './App.css';
+import React from "react";
+import { faBold, faItalic, faUnderline, faListUl, faListOl, faLink, faAlignLeft, faAlignRight, faAlignCenter, faAlignJustify, faImage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
 
 function App() {
+  const [ hasInitialized, setHasInitialized ] = React.useState(false);
+  window.addEventListener("load", () => {
+    if(hasInitialized) return;
+    window.addEventListener("keydown", (e) => {
+      if(e.ctrlKey) {
+        switch(e.keyCode) {
+          case 66:
+            document.execCommand("bold", false);
+            break;
+          case 73:
+            document.execCommand("italic", false);
+            break;
+          case 85:
+            document.execCommand("underline", false);
+            break;
+          default:
+            break;
+        }
+      }
+      if(e.ctrlKey && e.shiftKey) {
+        switch(e.keyCode) {
+          case 56:
+            document.execCommand("insertUnorderedList", false);
+            break;
+          case 55:
+            document.execCommand("insertOrderedList", false);
+            break;
+          default:
+            break;
+        }
+      }
+    });
+    setHasInitialized(true);
+  });
   return (
     <div className="App">
       <div className="_3ditor-main">
@@ -11,73 +45,62 @@ function App() {
         <div className="_3ditor-header">
           <button type="button" className="_3ditor-button" data-action="bold" onClick={() => {
             document.execCommand("bold", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faBold} />
+            <FontAwesomeIcon size="2x" icon={faBold} />
           </button>
           <button type="button" className="_3ditor-button" data-action="italic" onClick={() => {
             document.execCommand("italic", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faItalic} />
+            <FontAwesomeIcon size="2x" icon={faItalic} />
           </button>
           <button type="button" className="_3ditor-button" data-action="underline" onClick={() => {
             document.execCommand("underline", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faUnderline} />
+            <FontAwesomeIcon size="2x" icon={faUnderline} />
           </button>
           <button type="button" className="_3ditor-button" data-action="insertUnorderedList" onClick={() => {
             document.execCommand("insertUnorderedList", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faListUl} />
+            <FontAwesomeIcon size="2x" icon={faListUl} />
           </button>
           <button type="button" className="_3ditor-button" data-action="insertOrderedList" onClick={() => {
             document.execCommand("insertOrderedList", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faListOl} />
+            <FontAwesomeIcon size="2x" icon={faListOl} />
           </button>
           <button type="button" className="_3ditor-button" data-action="createLink" onClick={() => {
             let linkLocation = prompt("Please enter the link's target.", "https://www.example.com/");
             if(!linkLocation) return;
             document.execCommand("createLink", false, linkLocation);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faLink} />
+            <FontAwesomeIcon size="2x" icon={faLink} />
           </button>
           <button type="button" className="_3ditor-button" data-action="justifyLeft" onClick={() => {
             document.execCommand("justifyLeft", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faAlignLeft} />
+            <FontAwesomeIcon size="2x" icon={faAlignLeft} />
           </button>
           <button type="button" className="_3ditor-button" data-action="justifyCenter" onClick={() => {
             document.execCommand("justifyCenter", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faAlignCenter} />
+            <FontAwesomeIcon size="2x" icon={faAlignCenter} />
           </button>
           <button type="button" className="_3ditor-button" data-action="justifyRight" onClick={() => {
             document.execCommand("justifyRight", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faAlignRight} />
+            <FontAwesomeIcon size="2x" icon={faAlignRight} />
           </button>
           <button type="button" className="_3ditor-button" data-action="justifyFull" onClick={() => {
             document.execCommand("justifyFull", false);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faAlignJustify} />
+            <FontAwesomeIcon size="2x" icon={faAlignJustify} />
           </button>
           <button type="button" className="_3ditor-button" data-action="insertImage" onClick={() => {
             let imageLocation = prompt("Please enter the image's URL.", "https://www.example.com/example.png");
             if(!imageLocation) return;
             document.execCommand("insertImage", false, imageLocation);
-            (document.getElementsByClassName("_3editor-content") as HTMLCollectionOf<HTMLDivElement>)[0]?.focus();
           }}>
-            <FontAwesomeIcon icon={faImage} />
+            <FontAwesomeIcon size="2x" icon={faImage} />
           </button>
         </div>
         <div className="_3ditor-content" contentEditable={true}>
